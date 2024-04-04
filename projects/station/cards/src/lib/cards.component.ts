419f3c5171a3e75c8d21dfe349423a34c48307f2
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-04 16:48:03 -0300

    Cria a lib @station/cards

---
+++ projects/station/cards/src/lib/cards.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stc-cards',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      cards works!
+    </p>
+  `,
+  styles: ``
+})
+export class CardsComponent {
+
+}
---
+++ projects/station/cards/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of cards
+ */
+
+export * from './lib/cards.service';
+export * from './lib/cards.component';
